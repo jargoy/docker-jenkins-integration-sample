c2529b16ff035dfdf819fc7782a0f5bcc01ff142
unknown <yograjrajput027@gmail.com>
2026-05-10 19:29:51 +0530

    third commit

--- src/main/java/com/docker_jenkins_integration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/docker_jenkins_integration/DockerJenkinsIntegrationApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DockerJenkinsIntegrationApplication.class);
+    
+	@PostConstruct
+	public void init(){
+	logger.info("Application Started...");
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
